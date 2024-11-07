ed55ccc54b83a3410b884fbcda32e7ae1c34f2a6
FarhanSFix <farhansfixe@gmail.com>
2024-11-07 21:46:42 +0700

    create service and gelery

--- js/script.js
+++ js/script.js
@@ -12,3 +12,34 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+let slideIndex = 0;
+let slides = document.getElementsByClassName("slide");
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+function plusSlides(n) {
+  slideIndex += n;
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  } else if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+// Initialize slideshow
+showSlides();
